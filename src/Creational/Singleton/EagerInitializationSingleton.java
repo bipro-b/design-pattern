c6f3461ed51ef358748d553f9e505e3653f8404b
bipro <biprocuet@gmail.com>
2023-12-05 20:05:04 +0600

    Singleton

---
+++ src/Creational/Singleton/EagerInitializationSingleton.java
@@ -0,0 +1,15 @@
+package Creational.Singleton;
+
+public class EagerInitializationSingleton {
+
+    private static final EagerInitializationSingleton instance = new EagerInitializationSingleton();
+
+    private EagerInitializationSingleton(){}
+
+    public static EagerInitializationSingleton getInstance(){
+        return instance;
+    }
+
+
+}
+
