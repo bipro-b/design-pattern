81e7e82ca9b611298f810cf1674664a79030ffd7
bipro <biprocuet@gmail.com>
2023-12-05 20:42:39 +0600

    few update

--- src/Creational/Factory/TestFactory.java
+++ src/Creational/Factory/TestFactory.java
@@ -6,5 +6,14 @@ public class TestFactory {
         Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
         System.out.println("Factory PC Config::"+pc);
         System.out.println("Factory Server Config::"+server);
+
+        // IF it's not follow factory
+
+        Computer pc1 = new PC("20 GB","500 GB","2.6 GHz");
+        Computer server1 = new Server("16 GB","1 TB","3.9 GHz");
+
+        System.out.println("Factory PC Config::"+pc1);
+        System.out.println("Factory Server Config::"+server1);
+
     }
 }
