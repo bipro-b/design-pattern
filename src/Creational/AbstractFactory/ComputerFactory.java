23a6e340245f45cdb434de6ad310b1b111ce86da
bipro <biprocuet@gmail.com>
2023-12-05 21:35:51 +0600

    Abstract Factory

---
+++ src/Creational/AbstractFactory/ComputerFactory.java
@@ -0,0 +1,7 @@
+package Creational.AbstractFactory;
+
+public class ComputerFactory {
+    public static Computer getComputer(ComputerAbstractFactory factory){
+        return factory.createComputer();
+    }
+}
